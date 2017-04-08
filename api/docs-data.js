b291ee0cabc45fc61519e66f907e60e09cbc79fc
Haridu <haridu.senadeera@gmail.com>
2017-04-08 16:27:31 -0500

    serve requests to /docs

---
+++ api/docs-data.js
@@ -0,0 +1,36 @@
+const data = [
+  {
+    "id": 1,
+    "filename": "EECS-394-Homework.doc",
+    "create_date": "Tue Mar 24 2015 19:00:00 GMT-0500 (CDT)",
+    "last_updated": "Tue Mar 24 2015 21:20:00 GMT-0500 (CDT)",
+    "author_name": "John Smith",
+    "size": "656 KB"
+  },
+  {
+    "id": 2,
+    "filename": "English 101 Research Papaer.doc",
+    "create_date": "Wed Sep 24 2016 09:00:00 GMT-0500 (CDT)",
+    "last_updated": "Wed Sep 24 2016 12:00:00 GMT-0500 (CDT)",
+    "author_name": "Joycelyn Deaner",
+    "size": "790 KB"
+  },
+  {
+    "id": 3,
+    "filename": "SQLProgram1Fall2016Movies.doc",
+    "create_date": "Tue Mar 24 2015 01:00:00 GMT-0500 (CDT)",
+    "last_updated": "Sun Mar 24 2015 03:00:00 GMT-0500 (CDT)",
+    "author_name": "Branden Messier",
+    "size": "498 KB"
+  },
+  {
+    "id": 4,
+    "filename": "Resume.pdf",
+    "create_date": "Mon Feb 1 2015 19:00:00 GMT-0500 (CDT)",
+    "last_updated": "Tue Feb 2 2015 01:00:00 GMT-0500 (CDT)",
+    "author_name": "Merilyn Wilt",
+    "size": "234 KB"
+  },
+];
+
+module.exports = data;
