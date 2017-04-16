3f6e8403f2f712a7c7a94644b51e5214da1eebe8
Safia Abdalla <safia@safia.rocks>
2017-04-16 17:32:07 -0500

    Replace author name in files

--- api/docs-data.js
+++ api/docs-data.js
@@ -4,7 +4,7 @@ const data = [
     "filename": "EECS-394-Homework.doc",
     "create_date": "Tue Mar 24 2015 19:00:00 GMT-0500 (CDT)",
     "last_updated": "Tue Mar 24 2015 21:20:00 GMT-0500 (CDT)",
-    "author_name": "John Smith",
+    "author_name": "Me",
     "size": "656 KB"
   },
   {
@@ -12,7 +12,7 @@ const data = [
     "filename": "English 101 Research Papaer.doc",
     "create_date": "Wed Sep 24 2016 09:00:00 GMT-0500 (CDT)",
     "last_updated": "Wed Sep 24 2016 12:00:00 GMT-0500 (CDT)",
-    "author_name": "Joycelyn Deaner",
+    "author_name": "Me",
     "size": "790 KB"
   },
   {
@@ -20,7 +20,7 @@ const data = [
     "filename": "SQLProgram1Fall2016Movies.doc",
     "create_date": "Tue Mar 24 2015 01:00:00 GMT-0500 (CDT)",
     "last_updated": "Sun Mar 24 2015 03:00:00 GMT-0500 (CDT)",
-    "author_name": "Branden Messier",
+    "author_name": "Me",
     "size": "498 KB"
   },
   {
@@ -28,7 +28,7 @@ const data = [
     "filename": "Resume.pdf",
     "create_date": "Mon Feb 1 2015 19:00:00 GMT-0500 (CDT)",
     "last_updated": "Tue Feb 2 2015 01:00:00 GMT-0500 (CDT)",
-    "author_name": "Merilyn Wilt",
+    "author_name": "Me",
     "size": "234 KB"
   },
 ];
