73f1c7872d421a22713df40e0a20cde0c99c9c98
Safia Abdalla <safia@safia.rocks>
2017-04-16 19:12:26 -0500

    Add help page

---
+++ src/pages/help/help.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+import { NavController, NavParams } from 'ionic-angular';
+
+import { ItemDetailsPage } from '../item-details/item-details';
+
+
+@Component({
+  selector: 'page-list',
+  templateUrl: 'help.html'
+})
+export class HelpPage {
+  icons: string[];
+}
