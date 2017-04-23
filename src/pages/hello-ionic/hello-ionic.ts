4c03552f3fe8ff0c31dec2de88d631addc0c7940
LueptowK <kylelueptow@gmail.com>
2017-04-23 15:45:34 -0500

    Autocomplete progress
    incomplete autocomplete code

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -95,6 +95,8 @@ export class HelloIonicPage {
         }
 
         this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+        var input = document.getElementById('pac-input');
+        var autocomplete = new google.maps.places.Autocomplete(input);
 
         //var uploadDiv = document.createElement('div');
         //this.createUploadButton(uploadDiv, this.map);
@@ -105,6 +107,45 @@ export class HelloIonicPage {
         this.geoLocalize(latLng,infoWindow)
         infoWindow.getPosition()
         }
+
+        autocomplete.bindTo('bounds', this.map);
+
+
+        var marker = new google.maps.Marker({
+          map: this.map,
+          anchorPoint: new google.maps.Point(0, -29)
+        });
+
+        autocomplete.addListener('place_changed', function() {
+          marker.setVisible(false);
+          var place = autocomplete.getPlace();
+          if (!place.geometry) {
+            // User entered the name of a Place that was not suggested and
+            // pressed the Enter key, or the Place Details request failed.
+            console.log("No details available for input: '" + place.name + "'");
+            return;
+          }
+
+          // If the place has a geometry, then present it on a map.
+          if (place.geometry.viewport) {
+            this.map.fitBounds(place.geometry.viewport);
+          } else {
+            this.map.setCenter(place.geometry.location);
+            this.map.setZoom(17);  // Why 17? Because it looks good.
+          }
+          marker.setPosition(place.geometry.location);
+          marker.setVisible(true);
+
+          var address = '';
+          if (place.address_components) {
+            address = [
+              (place.address_components[0] && place.address_components[0].short_name || ''),
+              (place.address_components[1] && place.address_components[1].short_name || ''),
+              (place.address_components[2] && place.address_components[2].short_name || '')
+            ].join(' ');
+          }
+        });
+
         this.getPrinterLocations(this)
         // var printers = [[42.052000, -87.678000],
         //                 [42.052000, -87.679370],
