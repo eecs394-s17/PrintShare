dcc2e4d64ff98831ed3a6139bdd440cfeaa21694
cga815 <carolinealexander2018@u.northwestern.edu>
2017-04-30 19:11:41 -0500

    more printers in different colors

--- api/printer-data.js
+++ api/printer-data.js
@@ -4,24 +4,35 @@ const data = [
     "owner": 1,
     "location": [42.052936, -87.679330],
     "active": true,
+    "color": true,
   },
   {
     "id": 2,
     "owner": 2,
     "location": [42.055645, -87.679224],
     "active": true,
+    "color": false,
   },
   {
     "id": 3,
     "owner": 2,
     "location": [42.056968, -87.676241],
-    "active": false,
+    "active": true,
+    "color": true,
+  },
+  {
+    "id": 3,
+    "owner": 2,
+    "location": [42.052500, -87.679970],
+    "active": true,
+    "color": true,
   },
   {
     "id": 4,
     "owner": 3,
     "location": [42.054212, -87.676241],
-    "active": false,
+    "active": true,
+    "color": false,
   }
 ];
 
